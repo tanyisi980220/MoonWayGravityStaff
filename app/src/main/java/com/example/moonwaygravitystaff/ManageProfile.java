773bb4cb72f40fdf386f624e89529a61c1e969a4
GORDON9839 <gohcl-aa16@student.tarc.edu.my>
2019-12-11 05:26:10 +0800

    user profile ok

--- app/src/main/java/com/example/moonwaygravitystaff/ManageProfile.java
+++ app/src/main/java/com/example/moonwaygravitystaff/ManageProfile.java
@@ -1,21 +1,46 @@
 package com.example.moonwaygravitystaff;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.moonwaygravitystaff.Model.Staff;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ManageProfile extends AppCompatActivity {
+    EditText edtName;
+    TextView txtRole;
+    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_profile);
+        init();
+    }
+
+    private void init() {
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference staffRef = database.getReference("Staff");
+        edtName = findViewById(R.id.edtName);
+
+        txtRole = findViewById(R.id.txtRole);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Manage Profile");
@@ -31,8 +56,26 @@ public class ManageProfile extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(),ChangePassword.class);
+                startActivity(intent);
+            }
+        });
+
+        //initalize the user name
+        staffRef.child(firebaseUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+
+                    Staff staff = dataSnapshot.getValue(Staff.class);
+                    edtName.setText(staff.getName());
+                    txtRole.setText("Role :" + staff.getRole().toUpperCase());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
